995af31b601937f10540078cc624afdc26266f6f
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2022-05-25 00:18:43 +0200

    chore(send-currencies): added form

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { LandingComponent } from './home/landing/landing.component';
+import { FormComponent } from './form/form.component';
+import { InputComponent } from './elements/input/input.component';
 
 
 
 @NgModule({
   declarations: [
     NavComponent,
-    LandingComponent
+    LandingComponent,
+    FormComponent,
+    InputComponent
   ],
   imports: [
     CommonModule
