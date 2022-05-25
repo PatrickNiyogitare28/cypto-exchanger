9a6f6d62ea63d8c728e1b45d602227d67488df98
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2022-05-25 18:35:45 +0200

    chore(transactions): added fetch transactions

--- src/app/components/home/transactions/transactions.component.ts
+++ src/app/components/home/transactions/transactions.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Transaction } from 'src/app/types/transaction';
 import { CryptoWalletService } from 'src/app/services/crypto-wallet.service';
+import { formatAddress } from 'src/app/utils/format-address';
 
 @Component({
   selector: 'app-transactions',
@@ -7,15 +9,24 @@ import { CryptoWalletService } from 'src/app/services/crypto-wallet.service';
   styleUrls: ['./transactions.component.scss']
 })
 export class TransactionsComponent implements OnInit {
-  public transactions = [];
+  public transactions:Array<Transaction> = [];
 
   constructor(private readonly cryptoWalletService: CryptoWalletService) { }
 
   private getTransactions = async () => {
     const transactions: any = await this.cryptoWalletService.getAllTransactions();
     if(transactions.length < 1) return;
-    this.transactions = transactions || [];
-
+    transactions.forEach((transaction:any) => {
+      const transactionObj: Transaction = {
+        addressTo: formatAddress(transaction.receiver),
+        addressFrom: transaction.sender,
+        timeStamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
+        message: transaction.message,
+        keyword: transaction.keyword,
+        amount: parseInt(transaction.amount._hex) / (10 ** 18)
+      }
+      this.transactions.push(transactionObj);
+    });
   }
   ngOnInit(): void {
     this.getTransactions();
---
+++ src/app/types/transaction.d.ts
@@ -0,0 +1,8 @@
+export type Transaction = {
+    addressTo: string,
+    addressFrom: string,
+    timeStamp: string,
+    message: string,
+    keyword: string,
+    amount: number
+}
